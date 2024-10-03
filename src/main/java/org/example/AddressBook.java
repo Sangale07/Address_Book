1403fc515f45c12219f0d4d53d4e0efd35601235
rushi <rushikeshsangale57@gmail.com>
2024-10-03 21:41:45 +0530

    Search By city  Or State

--- src/main/java/org/example/AddressBook.java
+++ src/main/java/org/example/AddressBook.java
@@ -3,6 +3,7 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class AddressBook {
     private String bookName;
