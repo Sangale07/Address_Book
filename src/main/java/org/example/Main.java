03d38d6e2e7b9eac105dbb29931738e429cd52ba
rushi <rushikeshsangale57@gmail.com>
2024-10-03 15:36:44 +0530

    Multiple Address Book

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,22 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
+    private static final Map<String, AddressBook> addressBookMap = new HashMap<>();
+
     public static void main(String[] args) {
+        addAddressBook();
+    }
 
-
+    public static void addAddressBook() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Address Book Name:");
+        String bookName = scanner.nextLine();
+        AddressBook addressBook = new AddressBook(bookName);
+        addressBookMap.put(bookName, addressBook);
+        System.out.println("Address Book '" + bookName + "' added.");
     }
 }
